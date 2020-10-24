8954e6330838dc9d5c51648c678d7da8f51a8973
romilpatel13 <38512773+romilpatel13@users.noreply.github.com>
2020-10-24 11:06:29 -0400

    Add files via upload

---
+++ Pratice/src/Duplicate.java
@@ -0,0 +1,23 @@
+public class Duplicate {
+    public static void main(String argu[]) {
+
+        String str = "school is  ";
+        int k = 0;
+        char[] inp = str.toCharArray();
+
+        System.out.println("Duplicate Characters are:");
+
+        for (int i = 0; i < str.length(); i++)
+        {
+            for (int j = i + 1; j < str.length(); j++)
+            {
+                if (inp[i] == inp[j])
+                {
+                    System.out.println(inp[j]);
+                    k++;
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Pratice/src/Reverse.java
@@ -0,0 +1,22 @@
+public class Reverse {
+    public static void main(String[] args) {
+
+        int [] arr = new int [] {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        System.out.println("Original array: ");
+
+        for (int i = 0; i < arr.length; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+
+        System.out.println("Reverse Array: ");
+
+        for (int i = arr.length-1; i >= 0; i--)
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
